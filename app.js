1285b39b9cd0a05029f33165f6f21321ba2ca64b
mubbe127 <mubarek.tajudin@gmail.com>
2025-01-19 03:03:59 +0100

    another try cors

--- app.js
+++ app.js
@@ -13,17 +13,13 @@ import { checkPosition, startGame, storeUsername } from "./controller/controller
 
 const app = express();
 
-
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://wereiswaldo-mubbe127.netlify.app",
-];
-
-app.use(function (req, res, next) {
-  const origin = req.header("Origin");
-  if (allowedOrigins.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
-  }
+app.use(cors({
+  origin: ['https://wereiswaldo-mubbe127.netlify.app', "http://localhost:5173"], // Replace with your frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true, // No cookies or credentials involved
+}));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", ["https://wereiswaldo-mubbe127.netlify.app"] ); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
