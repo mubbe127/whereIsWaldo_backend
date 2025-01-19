80bbda8b222cc148f9b87f6c4a54cc1bc60118bc
mubbe127 <mubarek.tajudin@gmail.com>
2025-01-19 18:39:36 +0100

    testing

--- app.js
+++ app.js
@@ -22,6 +22,12 @@ app.use(cors({
   credentials: true, // No cookies or credentials 
 }));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://wereiswaldo-mubbe127.netlify.app"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
