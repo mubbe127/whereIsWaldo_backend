901ca88cb79825f4e5524031fa852f4a377eb3aa
mubbe127 <mubarek.tajudin@gmail.com>
2025-01-19 02:53:44 +0100

    modify cors

--- app.js
+++ app.js
@@ -18,8 +18,16 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true, // No cookies or credentials involved
 }));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", ["http://localhost:5173","https://wereiswaldo-mubbe127.netlify.app"] ); // update to match the domain you will make the request from
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://wereiswaldo-mubbe127.netlify.app",
+];
+
+app.use(function (req, res, next) {
+  const origin = req.header("Origin");
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
